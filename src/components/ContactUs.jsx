5d7fa895e6934bf40cb5a90c4ded3b6fccb9dfe0
sakshuu <sakshisjadhav.120@gmail.com>
2025-03-27 00:20:41 +0530

    updated Home

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,14 +1,70 @@
 import React from 'react'
-import { princess } from '../assets/img'
+import { princess, Vishwaicon } from '../assets/img'
+import { calls, clock, facebook, insta, location, mail } from '../assets/img/icons';
 
 const ContactUs = () => {
+  const ContactInfo = [
+    {
+      title: "Address",
+      description: "Laxmi Nagar, Moshi 412105",
+      image:location
+    },
+    {
+      title: "Mobile number",
+      description: "7030204848",
+      image:calls
+    },
+    {
+      title: "Accessibility",
+      description: "10:00 to 6:00",
+      image:clock
+    },
+    {
+      title: "Email",
+      description: "butterfly.sales23@gmail.com",
+      image:mail
+    }
+  ];
   return (<>
 <div className="row" style={{width:'100%' }}>
   <div className="col-5 " style={{width:'50%'  }}>
     <img src={princess} style={{width:'50vmax', height:'53vmax', marginLeft:'-10%'}}  alt="" />
   </div>
   <div className="col-6" style={{width:'50%' }}>
+<p>A project by: </p>
+<img src={Vishwaicon} alt="" />
+<h1>Contact Details</h1>
+<p>
+Have a question or need more details? Get in touch with us—we’re here to help! Reach out via call, email, or visit us, and let’s build something amazing together.
+</p>
 
+<div className="row">
+{ContactInfo.map((item, index) => ( 
+  <div className="col-6">
+    <div>
+      <div className="d-flex">
+        <div><img src={item.image} alt="" /></div>
+        <titlediv>
+<p>{item.title}</p>
+<p>{item.description}</p>
+        </titlediv>
+      </div>
+    </div>
+  </div>
+  ))}
+</div>
+<hr />
+<div className='d-flex gap-10'>
+
+<div style={{backgroundColor:'rgba(241, 238, 216, 0.8)'}}>
+
+<img src={insta}  width={100} alt="" />
+</div>
+<div style={{backgroundColor:'rgba(241, 238, 216, 0.8)'}}>
+
+<img src={facebook} width={100}  alt="" />
+</div>
+</div>
   </div>
 </div>
 
@@ -17,7 +73,6 @@ const ContactUs = () => {
       <div className="container">
         <form>
           <div className="row">
-            {/* First Row: Name, Contact Number, Email */}
             <div className="col-md-4 mb-3">
               <input
                 type="text"
